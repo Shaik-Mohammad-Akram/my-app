10dd272d5a5824627ce593c6b2af2d67e5114abc
Shaik-Mohammad-Akram <skmdakram2508@gmail.com>
2025-01-09 15:21:17 +0530

    implemented pagination and delete methods

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -15,7 +15,14 @@ export class VehicleService {
     return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?filter='+term)
   }
   getsoretedvehicles(column:string,order:string):Observable<any>{
-    return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy='+column+'&order'+order);
+    return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy='+column+'&order='+order);
 
+  }
+  getpaginatedvehicles(limit:number,page:number):Observable<any>{
+    return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?limit='+limit+'&page='+page);
+
+  }
+  deletevehicle(id:number):Observable<any>{
+    return this._httpClient.delete('https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/'+id);
   }
 }
--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -10,7 +10,10 @@ import { CarouselsComponent } from '../carousels/carousels.component';
 export class VehicleComponent {
   vehicles:any=[];
   constructor(private _vehicleService:VehicleService){
-    _vehicleService.getVehicles().subscribe(
+    this.pageload();
+  }
+  pageload(){
+    this._vehicleService.getVehicles().subscribe(
       (data:any)=>{
       this.vehicles=data;
        console.log(this.vehicles)
@@ -40,5 +43,27 @@ export class VehicleComponent {
         alert('internal server error')
       }
     )
+  }
+  limit:number=0;
+  page:number=0;
+  pagination(){
+    this._vehicleService.getpaginatedvehicles(this.limit,this.page).subscribe(
+      (data:any)=>{
+        this.vehicles=data;
+        console.log(this.vehicles);
+      },(err:any)=>{alert('internal server error')}
+    )
+  }
+  delete(id:number){
+    if(confirm('Are you sure to delete?')==true){
+      this._vehicleService.deletevehicle(id).subscribe(
+        (data:any)=>{
+          alert('Record Deleted succesfully');
+          this.pageload();
+        },(err:any)=>{
+          alert('Internal server error')
+        }
+      )
+    }
   }
 }

61cf54eb41e0e395989c494f04477fe12ce6de4e
Shaik-Mohammad-Akram <skmdakram2508@gmail.com>
2025-01-22 15:34:15 +0530

    implemted some login functionalitites

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  public loginForm:FormGroup =new FormGroup({
+    email:new FormControl(),
+    password:new FormControl()
+  }
+)
+  constructor(private _loginservice:LoginService,private _router:Router){}
+    login(){
+      console.log(this.loginForm.value);
+      this._loginservice.login(this.loginForm.value).subscribe(
+        (Data:any)=>{
+          console.log(Data);
+          alert('login successful')
+          this._router.navigateByUrl('/dashboard');
+        },(err:any)=>{
+          alert('invalid credentials')
+        }
+      )
+    }
+  
 }
